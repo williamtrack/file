bbef50f0b6234580dc0d160bbd5cb30869773d89
William <zingon@163.com>
2023-01-31 16:32:10 +0800

    0131

--- CHtime.cpp
+++ CHtime.cpp
@@ -1,6 +1,6 @@
 #include "CHtime.h"
-#include<iostream>
-#pragma warning(disable : 4996)
+
+#include <iostream>
 
 void CHtime::pt()
 {
@@ -9,42 +9,48 @@ void CHtime::pt()
 
 void CHtime::pt(const char* tag)
 {
-    std::cout << tag<<": ";
+    std::cout << tag << ": ";
     gettm(getTimeStamp());
 }
 
 std::string CHtime::gt()
 {
-	time_t milli = getTimeStamp()+ (time_t)8 * 60 * 60 * 1000;
-	auto mTime = std::chrono::milliseconds(milli);
-	auto tp = std::chrono::time_point<std::chrono::system_clock, std::chrono::milliseconds>(mTime);
-	auto tt = std::chrono::system_clock::to_time_t(tp);
-	std::tm* now = gmtime(&tt); //关闭不安全警告 #pragma warning(disable : 4996)
-	std::string result 
-		= std::to_string(now->tm_min) + ":"
-		+ std::to_string(now->tm_sec) + ":"
-		+ std::to_string(milli % 1000);
-	return result;
+    time_t milli = getTimeStamp() + (std::time_t)8 * 60 * 60 * 1000;
+    auto mTime = std::chrono::milliseconds(milli);
+    auto tp = std::chrono::time_point<std::chrono::system_clock, std::chrono::milliseconds>(mTime);
+    auto tt = std::chrono::system_clock::to_time_t(tp);
+
+    struct tm now;
+    gmtime_s(&now, &tt);
+    std::string result
+        = std::to_string(now.tm_min) + ":"
+        + std::to_string(now.tm_sec) + ":"
+        + std::to_string(milli % 1000);
+    return result;
 }
 
-time_t CHtime::getTimeStamp()
+std::time_t CHtime::getTimeStamp()
 {
     std::chrono::time_point<std::chrono::system_clock, std::chrono::milliseconds> tp = std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::system_clock::now());
     auto tmp = std::chrono::duration_cast<std::chrono::milliseconds>(tp.time_since_epoch());
-    time_t timestamp = tmp.count();
+    std::time_t timestamp = tmp.count();
     //std::time_t timestamp = std::chrono::system_clock::to_time_t(tp);
     return timestamp;
 }
 
-std::tm* CHtime::gettm(time_t timestamp)
+std::tm* CHtime::gettm(std::time_t timestamp)
 {
-    time_t milli = timestamp + (time_t)8*60*60*1000;//此处转化为东八区北京时间，如果是其它时区需要按需求修改
+    std::time_t milli = timestamp + (std::time_t)8*60*60*1000;//此处转化为东八区北京时间，如果是其它时区需要按需求修改
     auto mTime = std::chrono::milliseconds(milli);
     auto tp = std::chrono::time_point<std::chrono::system_clock, std::chrono::milliseconds>(mTime);
     auto tt = std::chrono::system_clock::to_time_t(tp);
-    std::tm* now = gmtime(&tt); //关闭不安全警告 #pragma warning(disable : 4996)
-	//std::tm* now = nullptr;
-	//gmtime_s(now,&tt);
-    printf("%02d:%02d:%02d.%d\n", now->tm_hour, now->tm_min, now->tm_sec, milli % 1000);
-    return now;
+
+    struct tm now;
+    gmtime_s(&now, &tt);
+    printf("%02d:%02d:%02d.%03d\n", now.tm_hour, now.tm_min, now.tm_sec, (int)(milli % 1000));
+    //printf("%03lld\n", milli);
+    
+    //std::tm* now = std::gmtime(&tt);
+    //printf("%4d年%02d月%02d日 %02d:%02d:%02d.%d\n", now->tm_year + 1900, now->tm_mon + 1, now->tm_mday, now->tm_hour, now->tm_min, now->tm_sec, milli % 1000);
+    return &now;
 }
